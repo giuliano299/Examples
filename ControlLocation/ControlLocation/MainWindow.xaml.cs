6735f90f15df2b54aeb3cdd6e096cfa87b89b0d1
Ilya Verbitskiy <iverbitskiy@yahoo.com>
2013-01-22 22:34:48 +0100

    Added an example how to find WPF Control Location

---
+++ ControlLocation/ControlLocation/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlLocation
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventManager.RegisterClassHandler(typeof(Button), MouseDownEvent, new RoutedEventHandler(OnMouseDown));
+        }
+
+        private void OnMouseDown(object sender, RoutedEventArgs e)
+        {
+            var element = sender as ContentControl;
+            if (element != null)
+            {
+                ShowLocation(element);
+            }
+        }
+
+        private void ShowLocation(ContentControl element)
+        {
+            var location = element.PointToScreen(new Point(0, 0));
+            MessageBox.Show(string.Format(
+                                          "{2}'s location is ({0}, {1})", 
+                                          location.X, 
+                                          location.Y, 
+                                          element.Content));
+        }
+    }
+}
